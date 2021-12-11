6ac8b12ce9f5311d66a985a79e28e546be758cd6
Brandon Ha <idevbrandon@gmail.com>
2021-12-11 15:56:00 +0900

    feat: handle inputbox with events

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -9,6 +9,17 @@ class SearchInput {
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
 
+    // 페이지 진입 시 포커스가 자동으로 input을 가리키게 처리
+    $searchInput.focus();
+
+    // 인풋 클릭시, 인풋 initalize 이벤트 발생
+    $searchInput.addEventListener("click", (e) => {
+      let searchTerm = e.target.value;
+      if (searchTerm.length > 0) {
+        $searchInput.value = "";
+      }
+    });
+
     $searchInput.addEventListener("keyup", (e) => {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
